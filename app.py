83cd114edc3908619e818b4540ed1fa633313f1c
Ritik-1101 <drakerespawn@gmail.com>
2024-06-12 18:02:48 -0400

    commit

--- app.py
+++ app.py
@@ -1,10 +1,15 @@
-print("Importing create_app from app")
-from project.main import create_app
+import os
+import sys
 
-print("Creating app instance")
+print("Current working directory:", os.getcwd())
+print("Python path:", sys.path)
+
+from app import create_app
+
 app = create_app()
 
 print("App instance created")
 
 if __name__ == '__main__':
+    print("Running app")
     app.run(debug=True, host='0.0.0.0')
