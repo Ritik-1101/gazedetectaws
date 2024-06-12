b7b9af815dcf40ccf7bd30416fdcc642dbb8b690
Ritik-1101 <drakerespawn@gmail.com>
2024-06-12 17:55:36 -0400

    commit

--- app.py
+++ app.py
@@ -1,6 +1,11 @@
+print("Importing create_app from app")
 from app import create_app
 
+print("Creating app instance")
 app = create_app()
 
+print("App instance created")
+
 if __name__ == '__main__':
+    print("Running app")
     app.run(debug=True, host='0.0.0.0')
